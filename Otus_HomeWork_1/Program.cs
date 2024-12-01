0721c115c26be26a300e805edd0411bc76a9fc38
Nikita Stepanov <nst175@gmail.com>
2024-12-01 16:43:48 +0100

    HW1 Refactoring

--- Otus_HomeWork_1/Program.cs
+++ Otus_HomeWork_1/Program.cs
@@ -1,114 +1,174 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+
+namespace Otus_HomeWork_1;
 
 internal class Program
 {
     public static void Main(string[] args)
     {
         var work = true;
-        string? user = null;
-        string? echoMessage = null;
+        var user = string.Empty;
+        var echoMessage = string.Empty;
 
-        Console.WriteLine("Welcome to home work bot.");
-        Console.WriteLine("Available options are: /start, /help, /info, /exit.");
-        Console.WriteLine();
+        WelcomeMessage();
 
         while (work)
         {
-            if (user != null) Console.WriteLine($"Hey, {user}.");
-            Console.WriteLine("Enter your command:");
+            var input = GetUserInput(user);
 
-            var input = Console.ReadLine();
-
-            if (input != null && input.StartsWith("/echo") && input.IndexOf(" ") != -1)
+            if (!string.IsNullOrEmpty(input) && input.StartsWith("/echo") && input.IndexOf(' ') != -1)
             {
-                echoMessage = input.Substring(input.IndexOf(" ") + 1);
+                echoMessage = input.Substring(input.IndexOf(' ') + 1);
                 input = "/echo";
             }
 
             switch (input)
             {
                 case "/start":
-                    if (user != null)
-                    {
-                        Console.Clear();
-
-                        var correctInputFlag = false;
-                        var exit = false;
-
-                        while (correctInputFlag == false)
-                        {
-                            Console.WriteLine("Do you want to reintroduce yourself? Press 'y' for yes or 'n' for no.");
-                            var key = Console.ReadKey();
-
-                            if (key.KeyChar == 'y' || key.KeyChar == 'Y')
-                            {
-                                user = null;
-                                Console.Clear();
-                                correctInputFlag = true;
-                            }
-                            else if (key.KeyChar == 'n' || key.KeyChar == 'N')
-                            {
-                                Console.Clear();
-                                correctInputFlag = true;
-                                exit = true;
-                            }
-                        }
-                        
-                        if (exit) break;
-                    }
-
-                    while (user == null)
-                    {
-                        Console.Clear();
-                        Console.WriteLine("Please enter your name:");
-                        user = Console.ReadLine();
-                        Console.Clear();
-                    }
-
+                    user = string.IsNullOrEmpty(user) ? Start() : Restart(ref user);
                     break;
                 case "/help":
-                    Console.Clear();
                     AvailableOptions(user);
                     break;
                 case "/echo":
-                    Console.Clear();
-
-                    if (user == null)
-                    {
-                        Console.WriteLine("Please introduce yourself first. Execute /start command.");
-                        Console.WriteLine();
-                        break;
-                    }
-
-                    Console.WriteLine($"Echo message: {echoMessage}");
-                    Console.WriteLine();
+                    Echo(user, echoMessage);
                     break;
                 case "/info":
-                    Console.Clear();
-                    Console.WriteLine("Version 1.0");
-                    Console.WriteLine("16.11.2024");
-                    Console.WriteLine();
+                    BotInfo();
                     break;
                 case "/exit":
                     work = false;
                     break;
                 default:
-                    Console.Clear();
-                    Console.WriteLine("Not a valid command. Type /help for a list of options.");
-                    Console.WriteLine();
+                    DefaultAction();
                     break;
             }
         }
     }
+    
+    private static string GetUserInput(string user)
+    {
+        if (!string.IsNullOrEmpty(user)) Console.WriteLine($"Hey, {user}.");
+        Console.WriteLine("Enter your command:");
+        return Console.ReadLine() ?? string.Empty;
+    }
 
+    private static void WelcomeMessage()
+    {
+        Console.WriteLine("Welcome to home work bot.");
+        Console.WriteLine("Available options are: /start, /help, /info, /exit.");
+        Console.WriteLine();
+    }
+
+    private static string Start()
+    {
+        string user;
+        
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Please enter your name:");
+            user = Console.ReadLine() ?? string.Empty;
+
+            if (!string.IsNullOrEmpty(user)) continue;
+            
+            Console.WriteLine("Name cannot be empty. Press any key to retry.");
+            Console.ReadKey();
+        } while (string.IsNullOrEmpty(user));
+
+        Console.Clear();
+        return user;
+    }
+
+    private static string Restart(ref string user)
+    {
+        Console.Clear();
+
+        while (true)
+        {
+            Console.WriteLine("Do you want to reintroduce yourself? Press 'y' for yes or 'n' for no.");
+            var key = Console.ReadKey();
+
+            switch (key.KeyChar)
+            {
+                case 'y':
+                case 'Y':
+                    user = Start();
+                    return user;
+                case 'n':
+                case 'N':
+                    Console.Clear();
+                    return user;
+                default:
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid input. Please press 'y' for yes or 'n' for no.");
+                    Console.ResetColor();
+                    break;
+            }
+        }
+    }
+    
     private static void AvailableOptions(string? user)
     {
-        Console.WriteLine("Available options are:");
-        Console.WriteLine("/start - Starts or restarts a dialogue.");
-        Console.WriteLine("/help - Display this help message.");
-        if (user != null) Console.WriteLine("/echo - Echoes a message.");
-        Console.WriteLine("/info - Display the information about this application.");
-        Console.WriteLine("/exit - Exit the program.");
+        var sb = new StringBuilder();
+        
+        sb.AppendLine("Available options are: ");
+        sb.AppendLine("/start - Starts or restarts a dialogue.");
+        sb.AppendLine("/help - Display this help message.");
+        if (!string.IsNullOrEmpty(user)) sb.AppendLine("/echo - Echoes a message.");
+        sb.AppendLine("/info - Display the information about this application.");
+        sb.AppendLine("/exit - Exit the program.");
+        
+        Console.Clear();
+        Console.WriteLine(sb.ToString());
+    }
+
+    private static void Echo(string user, string echoMessage)
+    {
+        Console.Clear();
+
+        if (string.IsNullOrEmpty(user))
+        {
+            Console.WriteLine("Please introduce yourself first. Execute /start command.");
+            Console.WriteLine();
+            return;
+        }
+
+        Console.WriteLine($"Echo message: {echoMessage}");
+        Console.WriteLine();
+    }
+
+    private static void BotInfo()
+    {
+        var version = "1.1";
+        var releaseDate = "16.11.2024";
+        var patchNotes = new List<string>
+        {
+            "Version 1.0 [16.11.2024] - Initial release.",
+            "Version 1.1 [01.12.2024] - Refactoring and code cleanup."
+        };
+
+        Console.Clear();
+        
+        var sb = new StringBuilder();
+        sb.AppendLine($"Version: {version}");
+        sb.AppendLine($"Release Date: {releaseDate}");
+        sb.AppendLine();
+        sb.AppendLine("Patch Notes:");
+
+        foreach (var note in patchNotes)
+        {
+            sb.AppendLine($"* {note}");
+        }
+
+        Console.WriteLine(sb.ToString());
+    }
+
+    private static void DefaultAction()
+    {
+        Console.Clear();
+        Console.WriteLine("Not a valid command. Type /help for a list of options.");
         Console.WriteLine();
     }
 }
